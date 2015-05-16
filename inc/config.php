eca0543b688dbb9b18cd2a4d2ac75ab9cecaabfe
John Cave <me@johncave.co.nz>
2015-05-16 16:01:26 +1200

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -9,8 +9,8 @@ $CONFIG = array (
 	"torwebdir" => "/torrents/",
 	"redisHost" => "127.0.0.1",
 	"redisPort" => 6379,
-	"scrapeCache" => 300,
-	"tableCache" => 40,
+	"scrapeCache" => 900, #Cache the individual scrapes for this many seconds.
+	"tableCache" => 180, #Cache the entire table for this many seconds.
 	"highlightColour" => "#E2266E",
 	"linkColour" => "#005C9D"
 );
