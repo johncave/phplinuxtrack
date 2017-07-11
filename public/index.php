77da2dfc3c80a515f676b6ec7ea7405b761ddb5a
PXgamer <owzie123@gmail.com>
2017-07-11 16:50:23 +0100

    Fixed config values mentioned in comment 314411418

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $oDotEnv = new Dotenv(__DIR__ . '/../config');
 $oDotEnv->load();
 
-$oDotEnv->required(['PROJECT_TITLE', 'LANGUAGE', 'TORRENT_DIRECTORY', 'TORRENTS_WEB_DIR', 'REDIS_HOST'])->notEmpty();
+$oDotEnv->required(['PROJECT_TITLE', 'LANGUAGE', 'TORRENT_DIRECTORY', 'TORRENT_WEB_DIRECTORY', 'REDIS_HOST'])->notEmpty();
 $oDotEnv->required(['REDIS_PORT', 'CACHE_SCRAPE', 'CACHE_TABLE'])->isInteger()->notEmpty();
 
 $oLanguage = new PhpLinuxTrack\Language();
